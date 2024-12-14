b37b3d1f5f1a8b006d467f766aca51a17529046c
Austin Songer, CISSP <a.songer@protonmail.com>
2024-12-13 20:00:16 -0600

    Update defaults.sh

--- macos-scripts/defaults.sh
+++ macos-scripts/defaults.sh
@@ -94,24 +94,25 @@ defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 # ENVIRONMENT SETUP
 #############################
 
-# Set colors to variables
-BLACK="\[\033[0;30m\]"
-BLACKB="\[\033[1;30m\]"
-RED="\[\033[0;31m\]"
-REDB="\[\033[1;31m\]"
-GREEN="\[\033[0;32m\]"
-GREENB="\[\033[1;32m\]"
-YELLOW="\[\033[0;33m\]"
-YELLOWB="\[\033[1;33m\]"
-BLUE="\[\033[0;34m\]"
-BLUEB="\[\033[1;34m\]"
-PURPLE="\[\033[0;35m\]"
-PURPLEB="\[\033[1;35m\]"
-CYAN="\[\033[0;36m\]"
-CYANB="\[\033[1;36m\]"
-WHITE="\[\033[0;37m\]"
-WHITEB="\[\033[1;37m\]"
-RESET="\[\033[0;0m\]"
+echo "Generating a new SSH key for GitHub..."
+
+# Generating a new SSH key
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/id_ed25519
+
+# Adding your SSH key to the ssh-agent
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+eval "$(ssh-agent -s)"
+
+touch ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+
+ssh-add -K ~/.ssh/id_ed25519
+
+# Adding your SSH key to your GitHub account
+# https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
+echo "run 'pbcopy < ~/.ssh/id_ed25519.pub' and paste that into GitHub"
+
 
 #############################
 # ITEMS THAT MUST BE FALSE
